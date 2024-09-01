ea05bcf0bbe893a6d804b49d2f1069003fadf734
cybergirl00 <92691928+cybergirl00@users.noreply.github.com>
2024-09-01 02:57:40 +0100

    first commit

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,12 @@
 declare type User = {
     name: string,
     email: string,
-    phone_number?: string,
+    phone_number?: string | null,
     username: string,
     clerkId: string,
     balance?: string,
-    accountNumber?: string, 
-    bankName?: string,
+    accountNumber?: string | null, 
+    bankName?: string | null,
     first_name: string,
     last_name: string,
     imageUrl: string
