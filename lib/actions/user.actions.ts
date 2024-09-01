f7eee60538a0e5f78511fdf69e4415401f8ddb7d
cybergirl00 <92691928+cybergirl00@users.noreply.github.com>
2024-09-01 03:37:56 +0100

    first commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,22 +3,22 @@ import { client } from "../client"
 
 export const createUser = async (user: User) => {
     try {
-        const createdUser = await client.user.create({
+        const newUser = await client.user.create({
             data: {
               name: user.name,
               email: user.email,
               clerkId: user.clerkId,
-              ...(user.username && { username: user.username }),
-              ...(user.imageUrl && { imageUrl: user.imageUrl }),
-              ...(user.first_name && { first_name: user.first_name }),
-              ...(user.last_name && { last_name: user.last_name }),
-              ...(user.phone_number && { phone_number: user.phone_number }),
-              ...(user.accountNumber && { accountNumber: user.accountNumber }),
-              ...(user.balance && { balance: user.balance }),
-              ...(user.bankName && { bankName: user.bankName }),
+              username: user.username || undefined,
+              imageUrl: user.imageUrl || undefined,
+              first_name: user.first_name || undefined,
+              last_name: user.last_name || undefined,
+              phone_number: user.phone_number || undefined,
+              accountNumber: user.accountNumber || undefined,
+              balance: user.balance !== undefined ? user.balance : undefined,  // Handle balance correctly
+              bankName: user.bankName || undefined,
             }
           });
-          return createdUser;          
+                     return JSON.parse(JSON.stringify(newUser));          
 
     } catch (error) {
         // todo add toast
